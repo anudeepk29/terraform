77ed4f3a28eca539cfd0f411a31aeb8ba0fb0d0b
anudeepk29 <46772745+anudeepk29@users.noreply.github.com>
2024-09-16 00:34:32 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+# main.tf
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+variable "project_id" {}
+variable "region" {}
